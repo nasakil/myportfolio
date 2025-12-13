b6c18c5c7e9e7c7ff923db73f69e699fcc6d7d8e
norman <norman.asakil@hcdc.edu.ph>
2025-12-13 21:30:45 +0800

    commit

--- norms/vite.config.js
+++ norms/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
+    base: `/${env.DG}/`,
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
